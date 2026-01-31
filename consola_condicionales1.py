e4d1876a38964247e02a76b24b060b3ee8cf65c3
Alison Daiana Aristizabal <a.aristizabalg@uniandes.edu.co>
2026-01-31 15:39:45 -0500

    Consola

---
+++ consola_condicionales1.py
@@ -0,0 +1,36 @@
+#TODO: importar el módulo
+import modulo_logica as ml
+def ejecutar_bisiesto()->None:
+    print("Vamos a decidir si un año es bisiesto o no")
+    #TODO: completar
+    anio=input("ingrese el año que quiere consultar: ")
+    bisiesto=ml.bisiesto(anio)
+    print(bisiesto)
+
+def ejecutar_clasificar()->None:
+    print("Vamos a determinar de qué tipo es un triángulo dados sus ángulos")
+    #TODO: completar
+
+def ejecutar_solucionar()->None:
+    print("Vamos a tratar de hallar las soluciones de una ecuación cuadrática")
+    #TODO: completar
+
+def mostrar_menu()->None:
+    print ("Menu principal")
+    print ("(1) Año bisiesto")
+    print ("(2) Tipo de triángulo")
+    print ("(3) Solución ecuación cuadrática")
+
+    x = input("Seleccione su opción: ")
+
+    #TODO: ejecuatar una funcionalidad dada la opción seleccionada
+
+
+def iniciar_aplicacion()->None:
+    print("Bienvenido al laboratorio de condicionales")
+    mostrar_menu()
+
+#PROGRAMA PRINCIPAL
+iniciar_aplicacion()
+
+    
